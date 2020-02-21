a85721f63cb73063f44e44980f9edf0fe2b1794f
Zakarya <zakarya.elkhattabi@gmail.com>
2020-02-21 18:15:23 +0100

    Création des classes Article/Comment et implémenation de l'héritage

--- libraries/database.php
+++ libraries/database.php
@@ -1,15 +1,14 @@
 <?php
 
 // Retourne une connnexion à la base de données
-
-
 function getPdo(): PDO{
 
-      $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
-         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+            $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', 
+                        [
+                            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                        ] );
 
-    return $pdo;
-
+            return $pdo;
 }
+
